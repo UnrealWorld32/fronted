71c0d11494ec2090f58a077b7d6d094ab5d76db9
Davitojr <59208431+Davitojr@users.noreply.github.com>
2022-11-25 17:47:35 -0600

    a

--- Javascript1/presupuesto/js/app.js
+++ Javascript1/presupuesto/js/app.js
@@ -1,11 +1,23 @@
 var ingresos = [
-    {Salario: 20000},
-    {VentaAuto: 50000}
+    {
+        descripcion: "Salario",
+        valor: 20000
+    },
+    {
+        descripcion: "Venta de Auto",
+        valor: 50000
+    }
 ]
 
 var egresos = [
-    Renta = 4000,
-    Ropa = 800
+    {
+        descripcion: "Renta",
+        valor: 4000
+    },
+    {
+        descripcion: "Ropa",
+        valor: 800
+    }
 ]
 
 var descripcion = $('#descripcion');
@@ -13,6 +25,8 @@ var valor = $('#valor');
 
 var cargarApp = () => {
     cargarCabecero();
+    cargarIngresos();
+    cargarEgresos();
 }
 
 var cargarCabecero = () => {
@@ -27,7 +41,7 @@ var cargarCabecero = () => {
 var totalIngresos = (ingresados) => {
     var totalIngreso = 0;
     for (let ingresa of ingresados) {
-        totalIngreso += parseInt(ingresa);
+        totalIngreso += parseInt(ingresa.valor);
     }
     return totalIngreso;
 }
@@ -35,7 +49,7 @@ var totalIngresos = (ingresados) => {
 var totalEgresos = (egresados) => {
     var totalEgreso = 0;
     for (let egresa of egresados) {
-        totalEgreso += egresa;
+        totalEgreso += egresa.valor;
     }
     return totalEgreso;
 }
@@ -56,13 +70,52 @@ var agregar = () => {
     var tipo = $("#tipo :selected").val();
     const nuevo = {
         descripcion: descripcion.val(),
-        valor: valor.val()
+        valor: parseInt(valor.val())
     };
     (tipo === 'Ingreso') ? ingresos.push(nuevo) : egresos.push(nuevo);
-    console.log(totalIngresos(ingresos[0]));
-    console.log(totalIngresos(ingresos[1]));
-    console.log(totalIngresos(ingresos[2]));
-    console.log(tipo);
+    cargarCabecero();
+    cargarIngresos();
+    cargarEgresos();
+    limpiarInputs();
+}
+
+var limpiarInputs = () => {
+    descripcion.val('');
+    valor.val('');
+}
+
+var cargarIngresos = () => {
+    $('#lista_ingresos').empty();
+    ingresos.map((elemento, indice) => {
+        const botonEliminar = tag({
+            tag: 'ion-icon',
+            attrs: {
+                name: "close-circle-outline",
+                role: "img",
+                class: "md hydrated",
+                arialabel: "close circle outline",
+                onclick: `eliminarIngreso(${indice})`
+            }
+        })('')
+        $('#lista_ingresos').append(escribirElemento([describir(elemento.descripcion), derechar([sumar(elemento.valor), cuadroEliminar(botonEliminar)])]));
+    })
+}
+
+var cargarEgresos = () => {
+    $('#lista_egresos').empty();
+    egresos.map((elemento, indice) => {
+        const botonEliminar = tag({
+            tag: 'ion-icon',
+            attrs: {
+                name: "close-circle-outline",
+                role: "img",
+                class: "md hydrated",
+                arialabel: "close circle outline",
+                onclick: `eliminarEgreso(${indice})`
+            }
+        })('')
+        $('#lista_egresos').append(escribirElemento([describir(elemento.descripcion), derechar([restar(elemento.valor), porcentar(elemento.valor), cuadroEliminar(botonEliminar)])]));
+    })
 }
 
 //  Toma de riesgos
@@ -76,6 +129,23 @@ const atrsTag = obj => (contenido = '') =>
 
 const tag = t => typeof t === 'string' ? atrsTag({ tag: t }) : atrsTag(t);
 
-var actualizarTotales = () => {
-    let 
+var elementoTag = elementos => tag({ tag: 'div', attrs: { class: 'elemento limpiarEstilos' } })(elementos);
+var escribirElemento = elementos => elementoTag(elementos);
+var describir = elementos => tag({ tag: 'div', attrs: { class: 'elemento_descripcion' } })(elementos);
+var derechar = elementos => tag({ tag: 'div', attrs: { class: 'derecha limpiarEstilos' } })(elementos);
+var sumar = elementos => tag({ tag: 'div', attrs: { class: 'elemento_valor' } })('+' + formatoMoneda(elementos));
+var restar = elementos => tag({ tag: 'div', attrs: { class: 'elemento_valor' } })('-' + formatoMoneda(elementos));
+var porcentar = elementos => tag({ tag: 'div', attrs: {class: 'elemento_porcentaje'}})(formatoPorcentaje(elementos / totalIngresos(ingresos)));
+var cuadroEliminar = elementos => tag({ tag: 'button', attrs: { class: 'elemento_eliminar_btn' } })(elementos);
+
+var eliminarIngreso = (elemento) => {
+    ingresos.splice(elemento, 1);
+    cargarCabecero();
+    cargarIngresos();
+}
+
+var eliminarEgreso = (elemento) => {
+    egresos.splice(elemento, 1);
+    cargarCabecero();
+    cargarEgresos();
 }
